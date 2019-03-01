8463b004136ad7d4903afb1fabf72513f4ef80f6
Chris Honda <christopher.honda92@gmail.com>
2019-03-01 12:57:01 -0800

    Fleshed out more links for nav

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3f040f4295b7fe7d2b77";
+/******/ 	var hotCurrentHash = "00939eeaf17ce4da0fc8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
