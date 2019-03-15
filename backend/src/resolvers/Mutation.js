b388dd9ae1bb9651328bba072b07382e2c491846
Chris Honda <christopher.honda92@gmail.com>
2019-03-14 21:05:04 -0700

    Fix typo in schema file and build out createItem mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,17 +1,16 @@
 const Mutations = {
-//     async createItem(parent, args, ctx, info) {
-//     const item = await ctx.db.mutation.createItem(
-//         {
-//             data: {
-//                 ...args
-//             }
-//         },
-//         info
-//     );
-
-//     return item;
-//     }
-
+    async createItem(parent, args, ctx, info) {
+        const item = await ctx.db.mutation.createItem(
+            {
+                data: {
+                ...args
+                }
+            },
+            info
+        );
+        console.log(item);
+        return item;
+    }
 };
 
 module.exports = Mutations;
